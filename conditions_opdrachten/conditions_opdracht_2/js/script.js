2bd76c6fc713ce7878929976d4409a9cd7f5f4f6
donnevspankeren <donne.van.spankeren01@gmail.com>
2019-12-03 10:55:02 +0100

    opdracht 2

--- conditions_opdrachten/conditions_opdracht_2/js/script.js
+++ conditions_opdrachten/conditions_opdracht_2/js/script.js
@@ -1,1 +1,10 @@
 //javascript code
+function myFunction(){
+    var Saldo = parseInt(document.getElementById("saldo").value);
+    
+    if (Saldo >= 25){
+        alert("Je hebt voldoende saldo om 25 euro op te nemen.")
+    } else{
+        alert("Sorry, je hebt te weinig saldo!")
+    }
+}
