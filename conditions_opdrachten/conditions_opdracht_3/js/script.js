ca690a6cdbd58f509a0b7c2908b843a9bacfdbb7
donnevspankeren <donne.van.spankeren01@gmail.com>
2019-12-03 11:01:44 +0100

    opdracht 3

--- conditions_opdrachten/conditions_opdracht_3/js/script.js
+++ conditions_opdrachten/conditions_opdracht_3/js/script.js
@@ -1,1 +1,12 @@
 //javascript code
+function myFunction(){
+    var Saldo = parseInt(document.getElementById("saldo").value);
+    
+    if (Saldo == 500){
+        alert(" Uw salaris is gestort.")
+    }  else if ( Saldo >= 500){
+        alert("U salaris is gestort en U heeft een bonus gekregen!")
+    } else {
+        alert("U salaris is nog niet gestort.")
+    }
+}
