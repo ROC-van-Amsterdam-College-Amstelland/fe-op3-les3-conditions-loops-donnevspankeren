71b4ec0c25d68eb3796e29b72a8e4d1327d3c9ae
donnevspankeren <donne.van.spankeren01@gmail.com>
2019-12-03 10:45:14 +0100

    opdracht 1

--- conditions_opdrachten/conditions_opdracht_1/js/script.js
+++ conditions_opdrachten/conditions_opdracht_1/js/script.js
@@ -1,1 +1,10 @@
 //javascript code
+function myFunction(){
+    var Saldo = parseInt(document.getElementById("saldo").value);
+    
+    if (Saldo < 25){
+        alert("niet genoeg saldo.")
+    } else{
+        alert("neem geld.")
+    }
+}
