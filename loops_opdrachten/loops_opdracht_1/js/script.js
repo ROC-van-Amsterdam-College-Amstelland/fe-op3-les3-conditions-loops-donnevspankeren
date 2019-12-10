0b82ac943e28182689767b292365636f8cadc788
donnevspankeren <donne.van.spankeren01@gmail.com>
2019-12-10 11:14:53 +0100

    opdracht 2

--- loops_opdrachten/loops_opdracht_1/js/script.js
+++ loops_opdrachten/loops_opdracht_1/js/script.js
@@ -1,7 +1,8 @@
 //javascript code
-var lijst = 0;
-for(i=0; i <= 5; i++){
+var lijst = 20;
+for(i= 19; i > 0; i--){
 console.log(i)
-lijst = lijst + i +"<br>";
+lijst = lijst + "<br>" + i;
 document.getElementById("output").innerHTML = lijst;
+
 }
--- loops_opdrachten/loops_opdracht_2/js/script.js
+++ loops_opdrachten/loops_opdracht_2/js/script.js
@@ -1,1 +1,8 @@
 //javascript code
+var lijst = 20;
+for(i= 19; i > 0; i--){
+console.log(i)
+lijst = lijst + "<br>" + i;
+document.getElementById("output").innerHTML = lijst;
+
+}
